95ed92d41fadad9a5af232a984a0c305f11b612f
huhan <117346021@qq.com>
2021-02-09 01:48:07 +0800

    2.9.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,60 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+//跳转到 tabBar 页面，并关闭其他所有非 tabBar 页面
+//在小程序插件中使用时，只能在当前插件的页面中调用
+  switchTab(){
+    wx.switchTab({
+      url: '/pages/logs/logs',
+      success: (res) => {
+        console.log(res)
+      },
+      fail: (res) => {
+        console.log(res)
+      },
+      complete: (res) => {},
+    })
+  },
+  //关闭所有页面，打开到应用内的某个页面
+  reLaunch(){
+    wx.reLaunch({
+      //如何接收参数，在哪里接收。为什么需要参数
+      url: '/pages/detail/detail?msg=666',
+      success: (res) => {
+        console.log(res)
+      },
+      fail: (res) => {
+        console.log(res)
+      },
+      complete: (res) => {
+        console.log(res)
+      },
+    })
+  },
+  navigatorTO(){
+    wx.navigateTo({
+      url: '/pages/detail/detail?name=hh&age=23',
+      events: 555,
+      success: (result) => {
+      setTimeout(() => {
+        wx.navigateBack({
+          delta: 1,
+          success: (res) => {},
+          fail: (res) => {},
+          complete: (res) => {
+            console.log(res)
+          },
+        })
+      }, 2000)
+      },
+      fail: (res) => {
+        console.log(res)
+      },
+      complete: (res) => {
+        console.log(res)
+      },
+    })
+  },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
